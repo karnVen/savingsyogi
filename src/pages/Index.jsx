7a692e8d6010891e99e831451b5b144d7d17eb9f
karnVen <onemonth06@gmail.com>
2026-01-17 20:35:14 +0530

    markettikcker

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -5,6 +5,7 @@ import { FeaturesSection } from '@/components/FeaturesSection'
 import { InsightsSection } from '@/components/InsightsSection'
 import { Footer } from '@/components/Footer'
 import Preloader from '@/components/home/Preloder'
+import MarketTicker from '@/components/MarketTicker' // 1. Import Added Here
 import { useState, useCallback } from 'react'
 
 const Index = () => {
@@ -13,12 +14,20 @@ const Index = () => {
   const handlePreloadComplete = useCallback(() => {
     setIsLoading(false)
   }, [])
+
   return (
     <main className="min-h-screen">
       {isLoading && <Preloader onComplete={handlePreloadComplete} />}
       {!isLoading && (
         <>
           <Navbar />
+          
+          {/* 2. Added Ticker with "Margin Top" to clear the Fixed Navbar */}
+          {/* These margins (mt-14/16/20) match the exact height of your Navbar */}
+          <div >
+            <MarketTicker />
+          </div>
+
           <Hero />
           <StatsBar />
           <FeaturesSection />
